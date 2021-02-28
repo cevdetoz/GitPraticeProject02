7dce543c07c6f0b890387b967c34f6284dbbaff3
cevdetoz <cevdetoz047@gmail.com>
2021-02-28 10:44:47 +0200

    ilk commit

---
+++ src/gitPratice/Git.java
@@ -0,0 +1,13 @@
+package gitPratice;
+
+public class Git {
+
+	public static void main(String[] args) {
+		System.out.println("maruf ozkan");
+		int a=0;
+		//new proje
+		System.out.println(a);
+
+	}
+
+}
